976a0d3c82f6b38a85296b688716f2a1c3f0d950
Pr0tag0n1st <Tj_Martinez01@hotmail.com>
2018-03-01 09:45:05 -0700

    Includes and such

--- Megaman Cuphead thing/Megaman Cuphead thing/Source.cpp
+++ Megaman Cuphead thing/Megaman Cuphead thing/Source.cpp
@@ -1,3 +1,10 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <iostream>
 using namespace std;
 int main() {
