2ec4b5ad8aff80a67e93ff1ebb9fcdebd08705b1
Pr0tag0n1st <Tj_Martinez01@hotmail.com>
2018-03-01 09:59:34 -0700

    Game screen

--- Megaman Cuphead thing/Megaman Cuphead thing/Source.cpp
+++ Megaman Cuphead thing/Megaman Cuphead thing/Source.cpp
@@ -8,5 +8,18 @@
 #include <iostream>
 using namespace std;
 int main() {
+	int screenwidth = 1200;
+	int screenheight = 1000;
+	int playersize = 40;
+	al_init();
+	al_init_primitives_addon();
+	al_init_image_addon();
+	al_init_acodec_addon();
+	al_init_ttf_addon();
+	al_init_font_addon();
+	ALLEGRO_DISPLAY *display = al_create_display(screenwidth, screenheight);
+	ALLEGRO_BITMAP *player = al_create_bitmap(playersize, playersize);
+	al_set_target_bitmap(player);
+	al_clear_to_color(al_map_rgb(100, 100, 255));
 	cout << "Is this thing working" << endl;
 }
